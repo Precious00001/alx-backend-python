3eab9c31b26ed0f94cdc48569e9f4fa98c9ca813
root <lanremidelola@gmail.com>
2023-12-12 11:26:31 +0000

    upload files

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
-'''Task 1's module.
+''' Description: Import async_generator from the previous task and then write
+                 a coroutine called async_comprehension
+                 that takes no arguments.
+                 The coroutine uses async comprehension
+                 over async_generator
+                 to collect 10 random numbers,
+                 then returns the list of 10 random
+                 numbers.
 '''
+
 from typing import List
-from importlib import import_module as using
 
 # Import the async_generator coroutine from module '0-async_generator'
-async_generator = using('0-async_generator').async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    '''Creates a list of 10 numbers from a 10-number
-    generator using async comprehension.
+    '''Return a list of values yielded by async_generator
+    using async comprehension.
 
     Returns:
-    List[float]: A list containing 10 numbers generated asynchronously.
+    List[float]: A list containing 10 random numbers.
     '''
-    return [num async for num in async_generator()]
+    return [value async for value in async_generator()]
