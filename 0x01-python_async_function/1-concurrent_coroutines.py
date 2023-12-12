899f0a96a038921de73ff54ffb60c08f7a8ef39d
root <lanremidelola@gmail.com>
2023-12-12 11:14:37 +0000

    upload files

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,9 +7,11 @@ from importlib import import_module as using
 # Import the async_generator coroutine from module '0-async_generator'
 async_generator = using('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List[float]:
-    '''Creates a list of 10 numbers from a 10-number generator using async comprehension.
-    
+    '''Creates a list of 10 numbers from a 10-number
+    generator using async comprehension.
+
     Returns:
     List[float]: A list containing 10 numbers generated asynchronously.
     '''
